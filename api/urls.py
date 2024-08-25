4648a9c3769b75df538dfba006991abdf7b49b9f
mudasirmattoo <120006081+mudasirmattoo@users.noreply.github.com>
2024-08-25 12:33:38 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,3 @@ from django.urls import path
 from .views import get_operation_code, process_data
 from api import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('bfhl', views.process_data, name='process_data'),
-    path('bfhl', views.get_operation_code, name='get_operation_code'),
-]
